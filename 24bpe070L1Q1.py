8fc0aefdf4acedec4b39eff5873019c6d38bd9c9
hetajmeri14 <24bpe070@spt.pdpu.ac.in>
2025-02-10 15:38:28 +0530

    Add files via upload

---
+++ 24bpe070L1Q1.py
@@ -0,0 +1,10 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a=5
+>>> b=6+
+SyntaxError: invalid syntax
+>>> a=5
+>>> b=6
+>>> a+b
+11
+>>> 
---
+++ 24bpe070L1Q4.py
@@ -0,0 +1,8 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #Dividing two numbeers
+>>> a=52
+>>> b=6
+>>> a/b
+8.666666666666666
+>>> 
